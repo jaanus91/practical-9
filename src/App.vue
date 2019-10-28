b5697fa9e0e8b640f030c6062de61d767e6139b2
Tsotne Kekelia <cotne.kekelia@yahoo.com>
2019-10-28 13:23:55 +0200

    Added assets

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Header :cart="cart"/>
+        <ItemPage :items="items"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import ItemPage from './components/ItemPage.vue'
+    import Header from './components/Header.vue'
+    import Cart from './models/Cart'
+    import Item from './models/Item'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+
+        name: 'app',
+        data: () => {
+            let mainItem = new Item(
+                "Programming books bundle",
+                50.0,
+                "image1.jpg",
+                [
+                    "image1.jpg",
+                    "image2.jpg",
+                    "image3.jpg",
+                    "image4.jpg"
+                ]
+            );
+            return {
+                cart: new Cart(),
+                items: [
+                    mainItem,
+                    new Item("Java Programming", 23.0, "image5.jpg"),
+                    new Item("Programming in C++", 15.0, "image6.jpg"),
+                    new Item("Programming and fundamentals of Python", 5.50, "image7.jpg"),
+                    new Item("My first coding book", 10.90, "image8.jpg")
+                ]
+            }
+        },
+        components: {
+            ItemPage,
+            Header,
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    html, body {
+        margin: 70px 0 0 0;
+        padding: 0;
+        background-color: #f7f7f7;
+    }
 </style>
